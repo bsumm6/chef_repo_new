663ab6de24b1d88ab4d8699763f5140e69818988
bsumm6 <ben.summers@coveros.com>
2016-07-18 10:26:00 -0400

    adding knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,3 +7,5 @@ node_name                "bsumm6"
 client_key               "#{current_dir}/bsumm6.pem"
 chef_server_url          "https://172.31.2.34/organizations/4thcoffee"
 cookbook_path            ["#{current_dir}/../cookbooks"]
+# Amazon AWS
+knife[:aws_credential_file] = "/home/centos/aws.txt"
