bba0d06e715a558e024e8954d1a34bda6277b517
Cloud User <centos@ip-172-31-2-23.ec2.internal>
2016-07-20 13:52:53 +0000

    adding knife.rb change

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,8 +3,8 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "bsumm6"
-client_key               "#{current_dir}/bsumm6.pem"
+node_name                "Ben_new_key"
+client_key               "#{current_dir}/Ben_new_key.pem"
 chef_server_url          "https://172.31.2.34/organizations/4thcoffee"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 # Amazon AWS
